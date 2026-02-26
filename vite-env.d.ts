f9918ec7c57a7840778399dfabb7ec7bdc1b2a09
Prakhar Kothari <kothariprakhar@gmail.com>
2026-02-26 11:36:39 -0600

    styling added

---
+++ vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly GEMINI_API_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_OWNER_EMAIL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
